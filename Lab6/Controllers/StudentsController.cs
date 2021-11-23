d69f192fd573236923d0eea88019443bb3280471
Tianyi Jian <jian0092@algonquinlive.com>
2021-11-22 21:58:39 -0500

    before create actions

---
+++ Lab6/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using Lab6.Data;
+using Lab6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly StudentsDbContext _context;
+        public StudentsController(StudentsDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get collection of Students.
+        /// </summary>
+        /// <returns>A collection of Students</returns>
+        /// <response code="200">Returns a collection of Students</response>
+        /// <response code="500">Internal error</response>      
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<Student>>> Get()
+        {
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
